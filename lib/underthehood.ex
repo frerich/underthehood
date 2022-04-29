1ece76fe583949a6cfc0ca12467fdb0e075605ce
Frerich Raabe <frerich.raabe@gmail.com>
2022-04-29 11:04:24 +0200

    Make Credo happy

--- lib/underthehood.ex
+++ lib/underthehood.ex
@@ -4,17 +4,19 @@ defmodule Underthehood do
   """
   use Phoenix.Component
 
+  import Phoenix.LiveView.Helpers, only: [live_component: 1]
+
   def terminal_button(assigns) do
     assigns
     |> Map.put_new(:id, :terminal_button)
-    |> Map.put(:module, Underthehood.TerminalButtonComponent)
-    |> Phoenix.LiveView.Helpers.live_component()
+    |> Map.put(:module, __MODULE__.TerminalButtonComponent)
+    |> live_component()
   end
 
   def terminal(assigns) do
     assigns
     |> Map.put_new(:id, :terminal)
-    |> Map.put(:module, Underthehood.TerminalComponent)
-    |> Phoenix.LiveView.Helpers.live_component()
+    |> Map.put(:module, __MODULE__.TerminalComponent)
+    |> live_component()
   end
 end
