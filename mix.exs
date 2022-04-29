c4f14ad1e26f0cffc71e122f38f2f94f40576011
Frerich Raabe <frerich.raabe@gmail.com>
2022-04-29 10:58:18 +0200

    Lower required Elixir version
    I think Elixir 1.12 ought to be just fine, this makes the library more
    widely usable.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Underthehood.MixProject do
     [
       app: :underthehood,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
