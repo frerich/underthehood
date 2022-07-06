c10070c0b5b41cd4fd7d5a9d61599c48ce0072d3
Frerich Raabe <frerich.raabe@gmail.com>
2022-07-06 08:25:11 +0200

    Bump version number to 0.1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Underthehood.MixProject do
     [
       app: :underthehood,
       description: description(),
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
