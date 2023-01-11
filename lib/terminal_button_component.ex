8ddef0e48ab9e5ff405ba6130e732b3a6831fd9e
Kevin Schweikert <kevin@kevinschweikert.de>
2023-01-11 13:21:18 +0100

    remove assigns warning from phoenix compiler

--- lib/terminal_button_component.ex
+++ lib/terminal_button_component.ex
@@ -6,23 +6,24 @@ defmodule Underthehood.TerminalButtonComponent do
 
   import Underthehood, only: [terminal: 1]
 
-  def render(%{id: id} = assigns) do
-    toggle_terminal =
-      JS.toggle(to: "##{id} .terminal_toggle_button")
-      |> JS.toggle(to: "##{id} .terminal_container")
-
-    terminal_id = "#{id}_terminal"
-
+  def render(%{id: _id} = assigns) do
     ~H"""
     <div id={@id} style="position: fixed; bottom: 1em; right: 1em; z-index: 2147483647;">
-      <div class="terminal_toggle_button" style="background-color: black; color: lightgray; padding: 0.3em; font-family: monospace; border-radius: 0.5em; cursor: pointer;" phx-click={toggle_terminal}>
+      <div class="terminal_toggle_button" style="background-color: black; color: lightgray; padding: 0.3em; font-family: monospace; border-radius: 0.5em; cursor: pointer;" phx-click={toggle_terminal(@id)}>
         iex>
       </div>
       <div class="terminal_container" style="display: none; position: relative; border: 2px solid lightgray;">
-        <.terminal id={terminal_id}/>
-        <div style="position: absolute; top: 8px; right: 8px; z-index: 2147483647; line-height: 1em; cursor: pointer; color: white;" phx-click={toggle_terminal}>&#x2715</div>
+        <.terminal id={terminal_id(@id)}/>
+        <div style="position: absolute; top: 8px; right: 8px; z-index: 2147483647; line-height: 1em; cursor: pointer; color: white;" phx-click={toggle_terminal(@id)}>&#x2715</div>
       </div>
     </div>
     """
   end
+
+  def terminal_id(id), do: "#{id}_terminal"
+
+  def toggle_terminal(id),
+    do:
+      JS.toggle(to: "##{id} .terminal_toggle_button")
+      |> JS.toggle(to: "##{id} .terminal_container")
 end
