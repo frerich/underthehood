79509d22f76d31bee431b2c54d7e20d20294a97b
Frerich Raabe <frerich.raabe@gmail.com>
2023-03-19 17:57:00 +0100

    Fix warnings

--- lib/terminal_button_component.ex
+++ lib/terminal_button_component.ex
@@ -6,23 +6,28 @@ defmodule Underthehood.TerminalButtonComponent do
 
   import Underthehood, only: [terminal: 1]
 
-  def render(%{id: id} = assigns) do
-    toggle_terminal =
-      JS.toggle(to: "##{id} .terminal_toggle_button")
-      |> JS.toggle(to: "##{id} .terminal_container")
+  def update(assigns, socket) do
+    terminal_id = "#{assigns.id}_terminal"
+    {:ok, assign(socket, :terminal_id, terminal_id)}
+  end
 
-    terminal_id = "#{id}_terminal"
-
+  def render(assigns) do
     ~H"""
     <div id={@id} style="position: fixed; bottom: 1em; right: 1em; z-index: 2147483647;">
-      <div class="terminal_toggle_button" style="background-color: black; color: lightgray; padding: 0.3em; font-family: monospace; border-radius: 0.5em; cursor: pointer;" phx-click={toggle_terminal}>
+      <div class="terminal_toggle_button" style="background-color: black; color: lightgray; padding: 0.3em; font-family: monospace; border-radius: 0.5em; cursor: pointer;" phx-click={toggle_terminal(@id)}>
         iex>
       </div>
       <div class="terminal_container" style="display: none; position: relative; border: 2px solid lightgray;">
-        <.terminal id={terminal_id}/>
-        <div style="position: absolute; top: 8px; right: 8px; z-index: 2147483647; line-height: 1em; cursor: pointer; color: white;" phx-click={toggle_terminal}>&#x2715</div>
+        <.terminal id={@terminal_id}/>
+        <div style="position: absolute; top: 8px; right: 8px; z-index: 2147483647; line-height: 1em; cursor: pointer; color: white;" phx-click={toggle_terminal(@id)}>&#x2715</div>
       </div>
     </div>
     """
+  end
+
+  defp toggle_terminal(component_id, js \\ %JS{}) do
+    js
+    |> JS.toggle(to: "##{component_id} .terminal_toggle_button")
+    |> JS.toggle(to: "##{component_id} .terminal_container")
   end
 end
